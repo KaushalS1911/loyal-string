2d297c032981ffd8a98e9e193d59489af2dfcf83
KaushalS1911 <kaushalsojitra19@gmail.com>
2024-12-10 16:50:35 +0530

    remove package module and disable eslint config

--- src/api/branch.js
+++ src/api/branch.js
@@ -2,11 +2,11 @@ import useSWR from 'swr';
 import { useMemo } from 'react';
 import { useAuthContext } from 'src/auth/hooks';
 import { fetcher } from '../utils/axios';
-import { ASSETS_API } from '../config-global';
+import { HOST_API } from '../config-global';
 
 export function useGetBranch() {
   const { user } = useAuthContext();
-  const URL = `${ASSETS_API}/api/company/${user?.company}/branch`;
+  const URL = `${HOST_API}/api/company/${user?.company}/branch`;
   const { data, isLoading, error, isValidating, mutate } = useSWR(URL, fetcher);
   const memoizedValue = useMemo(
     () => ({
