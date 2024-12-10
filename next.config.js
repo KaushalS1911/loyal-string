f88b6abaf8d313aa64b33ab8ecb480e55255dec2
KaushalS1911 <kaushalsojitra19@gmail.com>
2024-12-10 16:21:36 +0530

    remove package module and disable eslint config

--- next.config.js
+++ next.config.js
@@ -11,6 +11,11 @@ module.exports = {
       transform: '@mui/lab/{{member}}',
     },
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
