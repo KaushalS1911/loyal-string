4671c890809e89bdac3d5a67e93da5aaa54cacf9
SUJAL <sujalpaghadal17@gmail.com>
2024-12-12 18:42:49 +0530

    functiolity add

--- src/api/branch.js
+++ src/api/branch.js
@@ -6,22 +6,7 @@ import { HOST_API } from '../config-global';
 
 export function useGetBranch() {
   const { user } = useAuthContext();
-  const storedBranch = sessionStorage.getItem('selectedBranch');
-  let parsedBranch = storedBranch;
-
-  if (storedBranch !== 'all') {
-    try {
-      parsedBranch = JSON.parse(storedBranch);
-    } catch (error) {
-      console.error('Error parsing storedBranch:', error);
-    }
-  }
-
-  const branchQuery = parsedBranch && parsedBranch === 'all'
-    ? ''
-    : `branch=${parsedBranch}`;
-
-  const URL = `${HOST_API}/api/company/${user?.company}/branch?${branchQuery}`;
+  const URL = `${HOST_API}/api/company/${user?.company}/branch`;
   const { data, isLoading, error, isValidating, mutate } = useSWR(URL, fetcher);
   const memoizedValue = useMemo(
     () => ({
